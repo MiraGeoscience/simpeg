08d0b2ae3aa94b47e08f0d883d397618b20750be
fourndo <fourndo@gmail.com>
2023-05-31 11:10:53 -0400

    Experimenting with tutorial block size

--- tutorials/08-tdem/plot_inv_3_tem_3d.py
+++ tutorials/08-tdem/plot_inv_3_tem_3d.py
@@ -27,7 +27,7 @@ to simulate the fields at each time channel with sufficient accuracy.
 # Import Modules
 # --------------
 #
-
+from dask import array
 from discretize import TreeMesh
 from discretize.utils import mkvc, refine_tree_xyz
 from SimPEG import dask
@@ -36,7 +36,7 @@ from SimPEG import data, data_misfit, directives, maps, optimization, regulariza
 import SimPEG.electromagnetics.time_domain as tdem
 # from geoh5py.workspace import Workspace
 # from geoh5py.objects import Curve
-from geoapps.driver_base.utils import treemesh_2_octree
+# from geoapps.driver_base.utils import treemesh_2_octree
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -239,7 +239,7 @@ mesh.finalize()
 # Conductivity in S/m
 air_conductivity = np.log(1e-8)
 background_conductivity = np.log(1e-3)
-block_conductivity = np.log(1e+0)
+block_conductivity = np.log(1e+1)
 
 # Active cells are cells below the surface.
 ind_active = surface2ind_topo(mesh, topo_xyz)
@@ -262,7 +262,7 @@ model = background_conductivity * np.ones(mesh.n_cells)
 #         face + np.c_[-150, 0, -225]
 #     ]
 # )
-ind_block = model_builder.getIndicesBlock(np.r_[-100, -100, -150], np.r_[100, 100, -50], mesh.cell_centers)
+ind_block = model_builder.getIndicesBlock(np.r_[-50, -50, -150], np.r_[50, 50, -50], mesh.cell_centers)
 model[ind_block] = block_conductivity
 model = model[ind_active]
 
@@ -279,7 +279,7 @@ def plot_model(model):
         ax=ax1,
         normal="Y",
         grid=True,
-        clim=(-3, 0),
+        # clim=(-3, 0),
         pcolor_opts={"cmap": mpl.cm.viridis},
     )
     ax1.set_title("")
@@ -297,7 +297,7 @@ def plot_model(model):
         ax=ax2,
         grid=True,
         ind=60,
-        clim=(-3, 0),
+        # clim=(-3, 0),
         pcolor_opts={"cmap": mpl.cm.viridis},
     )
     ax2.set_title("Recovered model")
@@ -345,35 +345,44 @@ def reshape(values):
 
 
 # Predict data for a given model
+simulation.model = model
+
 dpred = simulation.dpred(model)
 floors = (
     np.ones_like(reshape(np.abs(dpred))) *
     np.median(reshape(np.abs(dpred)), axis=2).flatten()[:, None, None]
-) + 1e-16
+).flatten(order='F') + 1e-16
+
+floors = np.asarray(array.einsum('ij,ij->i',  simulation.Jmatrix,  simulation.Jmatrix)**0.5)
+
 noise = np.random.randn(dpred.shape[0]) * ( #1e-15)
             np.abs(dpred) * 0.02
 )
 data_object = data.Data(
     survey,
     dobs=dpred + noise,
-    noise_floor=floors.flatten(order='F'),
+    noise_floor=floors,
 )
 dmis = data_misfit.L2DataMisfit(simulation=simulation, data=data_object, model_map=maps.IdentityMap(nP=nC))
 reg = regularization.Sparse(
-    mesh, alpha_s=1.,  # Set alpha_s = 0 to remove the reference model
+    mesh, alpha_s=0.,  # Set alpha_s = 0 to remove the reference model
     indActive=ind_active,
     mapping=maps.IdentityMap(nP=nC),
     gradientType="total",
     mref=np.log(1e-3) * np.ones(nC)
 )
 opt = optimization.ProjectedGNCG(
-    maxIter=20,
+    maxIter=10,
     lower=-np.inf,
     upper=np.inf,
     maxIterLS=10,
     maxIterCG=40,
     tolCG=1e-4
 )
+
+# Write a conjugate graditent solver for the Hessian
+
+
 inv_prob = inverse_problem.BaseInvProblem(dmis, reg, opt)
 inv = inversion.BaseInversion(
     inv_prob, directiveList=[
@@ -390,12 +399,12 @@ inv = inversion.BaseInversion(
         # ),
         directives.Update_IRLS(
             max_irls_iterations=0,
-            coolingRate=3,
-            chifact_start=5.0,
-            chifact_target=5.0,
+            coolingRate=2,
+            chifact_start=1.0,
+            chifact_target=1.0,
         ),
         directives.UpdatePreconditioner(),
-        directives.BetaEstimate_ByEig(beta0_ratio=1e+2, method="old")
+        directives.BetaEstimate_ByEig(beta0_ratio=1e+0, method="old")
     ]
 )
 
@@ -407,7 +416,6 @@ mrec = inv.run(m0)
 recovered_conductivity_model_log10 = np.log10(plotting_map * mrec)
 
 plot_model(recovered_conductivity_model_log10)
-
 #######################################################
 # Optional: Export Data
 # ---------------------
