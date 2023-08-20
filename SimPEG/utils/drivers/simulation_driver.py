3f395b6f0a2430aa83bc9214d9f64c9a951b1952
fourndo <fourndo@gmail.com>
2023-08-20 12:32:55 -0700

    Create drivers for octree, simulation and examples

---
+++ SimPEG/utils/drivers/simulation_driver.py
@@ -0,0 +1,447 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from scipy.interpolate import LinearNDInterpolator
+from scipy.spatial import cKDTree
+
+from SimPEG.potential_fields import gravity
+from SimPEG import maps
+from SimPEG.survey import BaseSurvey
+from discretize import TreeMesh
+from discretize.utils import mesh_builder_xyz, refine_tree_xyz, active_from_xyz
+
+from SimPEG.simulation import BaseSimulation
+from SimPEG import utils
+
+
+class OctreeDriver:
+    """
+    An octree mesh creation driver.
+
+    Choices on the cell size, extent, depth_core and refinements are made
+    based on the receiver locations and topography if not provided.
+    """
+
+    _depth_core: tuple[float]
+    _locations: np.ndarray
+    _padding_distance: tuple[float]
+    _refinements: dict[np.ndarray, dict]
+
+    def __init__(
+        self,
+        locations: np.ndarray | list,
+        cell_size: tuple[float, float, float] | None = None,
+        depth_core: float = 0.0,
+        padding_distance: tuple[float] = (0.0, 0.0, 0.0),
+        refinements: dict[np.ndarray, dict] | None = None,
+    ):
+        self._mesh: TreeMesh | None = None
+        self._cell_size: tuple[float, float, float] | None = None
+
+        self.locations = locations
+        self.cell_size = cell_size
+        self.depth_core = depth_core
+        self.padding_distance = padding_distance
+        self.refinements = refinements
+
+    @property
+    def cell_size(self) -> tuple[float, float, float]:
+        """
+        The cell size of the mesh.
+
+        Defaults to the median nearest neighbour distance of the locations.
+        """
+        if self._cell_size is None:
+            tree = cKDTree(self.locations)
+            dists, _ = tree.query(self.locations, k=2)
+            self._cell_size = (np.median(dists[1]),) * 3
+
+        return self._cell_size
+
+    @cell_size.setter
+    def cell_size(self, values):
+        if not isinstance(values, (tuple, type(None))):
+            raise TypeError(
+                f"Attribute 'cell_size' must be of type tuple, not {type(values)}."
+            )
+
+        if not isinstance(values, type(None)):
+            if len(values) != 3:
+                raise ValueError(
+                    f"Attribute 'cell_size' must be of length 3, not {len(values)}."
+                )
+
+        self._cell_size = values
+
+    @property
+    def locations(self) -> np.ndarray:
+        """
+        The receiver locations for the simulation.
+        """
+        return self._locations
+
+    @locations.setter
+    def locations(self, locations: np.ndarray | None):
+        if not isinstance(locations, (np.ndarray, list, type(None))):
+            raise TypeError(
+                f"Attribute 'locations' must be of type np.ndarray, not {type(locations)}."
+            )
+
+        if isinstance(locations, list):
+            locations = np.vstack(locations)
+
+        if not isinstance(locations, type(None)):
+            if locations.shape[1] != 3:
+                raise ValueError(
+                    f"Attribute 'locations' must be of shape (n, 3), not {locations.shape}."
+                )
+
+        self._locations = locations
+
+    @property
+    def mesh(self) -> TreeMesh:
+        """
+        An octree mesh object for the simulation.
+        """
+        if self._mesh is None:
+            self._mesh = mesh_builder_xyz(
+                self.locations,
+                self.cell_size,
+                depth_core=self.depth_core,
+                mesh_type="tree",
+            )
+
+            for xyz, refinement in self.refinements.items():
+                self._mesh = refine_tree_xyz(
+                    self._mesh,
+                    xyz,
+                    method=refinement["type"],
+                    octree_levels=refinement["levels"],
+                    finalize=False,
+                )
+
+            self._mesh.finalize()
+
+        return self._mesh
+
+    @property
+    def refinements(self):
+        """
+        The refinements to apply to the mesh.
+
+        Defaults to 4 levels of 'surface' refinement on the locations.
+        """
+        if self._refinements is None:
+            self._refinements = {
+                self.locations: {
+                    "type": "surface",
+                    "levels": (4, 4, 4),
+                }
+            }
+        return self._refinements
+
+    @refinements.setter
+    def refinements(self, refinements: dict | None):
+        if not isinstance(refinements, (dict, type(None))):
+            raise TypeError(
+                f"Attribute 'refinements' must be of type dict, not {type(refinements)}."
+            )
+
+        for key, value in refinements.items():
+            if not isinstance(key, np.ndarray):
+                raise TypeError(
+                    f"Attribute 'refinements' must be of type np.ndarray, not {type(key)}."
+                )
+
+            if not isinstance(value, dict):
+                raise TypeError(
+                    f"Attribute 'refinements' must be of type dict, not {type(value)}."
+                )
+
+            if "type" not in value.keys() or "levels" not in value.keys():
+                raise KeyError(
+                    "Attribute 'refinements' must contain keys 'type' and 'levels'."
+                )
+
+        self._refinements = refinements
+
+
+class BaseOctreeSimulationDriver(ABC):
+    _mesh_driver: OctreeDriver
+    _receiver_locations: np.ndarray
+    _simulation_class: BaseSimulation
+    _survey_class: BaseSurvey
+    _topography: np.ndarray
+
+    """
+    Base driver class for generating a simulation, survey and mesh for a given
+    array or receivers and topography.
+
+
+    Parameters
+    ----------
+    cell_size: tuple[float]
+        The cell size of the mesh.
+    receiver_locations: np.ndarray
+        The receiver locations for the simulation.
+    refinements: tuple[int]
+        The number of refinements to apply to the mesh.
+    topography: np.ndarray
+        The topography locations defaulted to a Gaussian surface with twice
+
+
+    Attributes
+    ----------
+    active_cells: np.ndarray
+        Array of bool for the active cells.
+    mesh: TreeMesh
+        An octree mesh object for the simulation.
+    simulation: BaseSimulation
+        The simulation object.
+    survey: BaseSurvey
+        Generate the survey object for a given class type.
+    """
+
+    def __init__(
+        self,
+        receiver_locations: np.ndarray,
+        topography: np.ndarray,
+        mesh_driver: OctreeDriver | None = None,
+    ):
+        self._active_cells = None
+        self._mesh: TreeMesh | None = None
+        self._simulation: BaseSimulation | None = None
+        self._survey: BaseSurvey | None = None
+
+        self.mesh_driver = mesh_driver
+        self.receiver_locations = receiver_locations
+        self.topography: np.ndarray = topography
+
+    @property
+    def active_cells(self) -> np.ndarray:
+        """
+        Array of bool for the active cells.
+        """
+        if self._active_cells is None:
+            self._active_cells = active_from_xyz(self.mesh, self.topography)
+
+        return self._active_cells
+
+    @property
+    def mesh(self) -> TreeMesh:
+        """
+        An octree mesh object for the simulation.
+        """
+        if self._mesh is None:
+            self._mesh = self.mesh_driver.mesh
+
+        return self._mesh
+
+    @property
+    def mesh_driver(self):
+        """
+        The mesh driver.
+        """
+        if getattr(self, "_mesh_driver", None) is None:
+            self._mesh_driver = OctreeDriver(
+                self.locations,
+                refinements={
+                    self.receiver_locations: {
+                        "type": "surface",
+                        "levels": (4, 4, 4),
+                    },
+                    self.topography: {
+                        "type": "surface",
+                        "levels": (0, 0, 4),
+                    },
+                },
+            )
+        return self._mesh_driver
+
+    @mesh_driver.setter
+    def mesh_driver(self, driver: OctreeDriver):
+        if not isinstance(driver, (OctreeDriver, type(None))):
+            raise TypeError(
+                f"Attribute 'mesh_driver' must be of type OctreeDriver, not {type(driver)}."
+            )
+
+        self._mesh_driver = driver
+
+    @property
+    def locations(self):
+        """
+        Pointer to the receiver and/or transmitter locations for the simulation.
+        """
+        if self.receiver_locations is None:
+            raise AttributeError(
+                "Attribute 'receiver_locations' must be defined before calling 'locations'."
+            )
+
+        return self.receiver_locations
+
+    @property
+    def n_actives(self) -> int:
+        """
+        The number of active cells.
+        """
+        return int(self.active_cells.sum())
+
+    @property
+    @abstractmethod
+    def simulation(self):
+        """
+        The simulation object.
+        """
+
+    @property
+    @abstractmethod
+    def survey(self):
+        """
+        Generate the survey object for a given class type.
+        """
+
+    @property
+    def receiver_locations(self):
+        """
+        The receiver locations for the simulation.
+        """
+        return self._receiver_locations
+
+    @receiver_locations.setter
+    def receiver_locations(self, locations: np.ndarray | None):
+        if not isinstance(locations, (np.ndarray, type(None))):
+            raise TypeError(
+                f"Attribute 'receiver_locations' must be of type np.ndarray, not {type(locations)}."
+            )
+
+        self._receiver_locations = locations
+
+    @property
+    def topography(self):
+        """
+        The receiver locations for the simulation.
+        """
+        return self._topography
+
+    @topography.setter
+    def topography(self, locations: np.ndarray | None):
+        if not isinstance(locations, (np.ndarray, type(None))):
+            raise TypeError(
+                f"Attribute 'topography' must be of type np.ndarray, not {type(locations)}."
+            )
+
+        self._topography = locations
+
+
+class GravitySimulationDriver(BaseOctreeSimulationDriver):
+    _simulation_class = gravity.simulation.Simulation3DIntegral
+    _survey_class = gravity.survey.Survey
+
+    """
+    The gravity example driver.
+
+    **kwargs
+        See BaseExampleDriver.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @property
+    def survey(self):
+        """
+        The gravity survey object.
+        """
+        if self._survey is None:
+            receivers = gravity.receivers.Point(self.receiver_locations)
+            sources = gravity.sources.SourceField([receivers])
+            self._survey = self._survey_class(sources)
+
+        return self._survey
+
+    @property
+    def simulation(self):
+        """
+        The gravity simulation object.
+        """
+        if self._simulation is None:
+            simulation = self._simulation_class(
+                mesh=self.mesh,
+                survey=self.survey,
+                rhoMap=maps.IdentityMap(nP=self.n_actives),
+                ind_active=self.active_cells,
+            )
+            self._simulation = simulation
+
+        return self._simulation
+
+
+class ExampleDriver:
+    """
+    The example driver.
+    """
+
+    def __init__(
+        self,
+        driver: type(BaseOctreeSimulationDriver),
+        extent: float = 30,
+        grid_size: int = 20,
+        height: float = 5.0,
+        **kwargs,
+    ):
+        self.extent = extent
+        self.height = height
+        self.grid_size = grid_size
+
+        self.driver = driver(**kwargs)
+
+    @property
+    def topography(self) -> np.ndarray:
+        """
+        The topography locations defaulted to a Gaussian surface with twice
+        the data extent.
+        """
+        if self.driver.topography is None:
+            extent = self.extent * 2.0
+
+            xr = np.linspace(-extent, extent, self.grid_size * 2)
+            yr = np.linspace(-extent, extent, self.grid_size * 2)
+            x, y = np.meshgrid(xr, yr)
+
+            # Gaussian surface
+            z = -np.exp((x**2 + y**2) / 75**2)
+            self.driver.topography = np.c_[
+                utils.mkvc(x.T), utils.mkvc(y.T), utils.mkvc(z.T)
+            ]
+
+        return self.driver.topography
+
+    @property
+    def locations(self) -> np.ndarray:
+        """
+        The receiver locations for the simulation.
+        """
+        if self.driver.receiver_locations is None:
+            xy_locations = self.make_grid(self.extent, self.grid_size)
+            # Drape the locations on topo
+            z_interp = LinearNDInterpolator(
+                self.topography[:, :2], self.topography[:, 2]
+            )
+            elevation = z_interp(xy_locations) + self.height
+            self.driver.receiver_locations = np.c_[xy_locations, elevation]
+
+        return self.driver.receiver_locations
+
+    @staticmethod
+    def make_grid(extent, resolution) -> np.ndarray:
+        """
+        Make a grid of positions centered on origin.
+
+        :param extent: Horizontal extent of the grid.
+        :param resolution: Number of points along axes of the grid.
+        :return: x,y locations of the grid.
+        """
+        xr = np.linspace(-extent, extent, resolution)
+        yr = np.linspace(-extent, extent, resolution)
+        x, y = np.meshgrid(xr, yr)
+
+        return np.c_[x.flatten(), y.flatten()]
---
+++ examples/02-gravity/plot_inv_grav.py
@@ -0,0 +1,121 @@
+"""
+PF: Gravity: Tiled Inversion Linear
+===================================
+
+Invert data in tiles.
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from SimPEG import (
+    maps,
+    data,
+    data_misfit,
+    regularization,
+    optimization,
+    inverse_problem,
+    directives,
+    inversion,
+)
+from SimPEG.utils.drivers.simulation_driver import GravityExampleDriver
+from SimPEG import utils
+
+###############################################################################
+# Setup
+# -----
+#
+# Define the survey and model parameters
+#
+# Create a global survey and mesh and simulate some data
+#
+#
+driver = GravityExampleDriver()
+model = np.zeros(driver.mesh.nC)
+ind = utils.model_builder.getIndicesBlock(
+    np.r_[-10, -10, -30],
+    np.r_[10, 10, -10],
+    driver.mesh.gridCC,
+)[0]
+
+# Assign magnetization values
+model[ind] = 0.3
+
+# Remove air cells
+model = model[driver.active_cells]
+
+###############################################################################
+# Generate data and a misfit function
+# Compute linear forward operator and compute some data
+d = driver.simulation.fields(model)
+
+# Add noise and uncertainties
+# We add some random Gaussian noise (1nT)
+synthetic_data = d + np.random.randn(len(d)) * 1e-3
+wd = np.ones(len(synthetic_data)) * 1e-3  # Assign flat uncertainties
+data_object = data.Data(
+    driver.survey,
+    dobs=synthetic_data,
+    standard_deviation=wd,
+)
+misfit_function = data_misfit.L2DataMisfit(
+    data=data_object, simulation=driver.simulation
+)
+
+###############################################################
+# Create an inversion problem with regularization function
+#
+# Create a regularization
+reg = regularization.Sparse(driver.mesh, active_cells=driver.active_cells)
+
+# Add directives to the inversion
+opt = optimization.ProjectedGNCG(
+    maxIter=100, lower=-1.0, upper=1.0, maxIterLS=20, maxIterCG=10, tolCG=1e-3
+)
+invProb = inverse_problem.BaseInvProblem(misfit_function, reg, opt)
+betaest = directives.BetaEstimate_ByEig(beta0_ratio=1e-1)
+
+# Here is where the norms are applied
+# Use a threshold parameter empirically based on the distribution of
+# model parameters
+update_IRLS = directives.Update_IRLS(
+    f_min_change=1e-4,
+    max_irls_iterations=0,
+    coolEpsFact=1.5,
+    beta_tol=1e-2,
+)
+saveDict = directives.SaveOutputEveryIteration(save_txt=False)
+update_Jacobi = directives.UpdatePreconditioner()
+sensitivity_weights = directives.UpdateSensitivityWeights(everyIter=False)
+inv = inversion.BaseInversion(
+    invProb,
+    directiveList=[update_IRLS, sensitivity_weights, betaest, update_Jacobi, saveDict],
+)
+
+# Run the inversion
+m0 = np.ones(driver.n_actives) * 1e-4  # Starting model
+mrec = inv.run(m0)
+
+###############################################################################
+# Plot the true and recovered model with respective data maps.
+ax = plt.subplot(2, 2, 1)
+utils.plot_utils.plot2Ddata(driver.locations, synthetic_data, ax=ax, clim=[-1e-3, 1e-3])
+
+ax = plt.subplot(2, 2, 2)
+utils.plot_utils.plot2Ddata(
+    driver.locations, driver.simulation.fields(mrec), ax=ax, clim=[-1e-3, 1e-3]
+)
+
+# Create active map to go from reduce set to full
+inject_global = maps.InjectActiveCells(driver.mesh, driver.active_cells, np.nan)
+
+ax = plt.subplot(2, 2, 3)
+driver.mesh.plot_slice(inject_global * model, normal="Y", ax=ax, grid=True)
+ax.set_title("True")
+ax.set_aspect("equal")
+
+ax = plt.subplot(2, 2, 4)
+driver.mesh.plot_slice(inject_global * mrec, normal="Y", ax=ax, grid=True)
+ax.set_title("Recovered")
+ax.set_aspect("equal")
+plt.show()
