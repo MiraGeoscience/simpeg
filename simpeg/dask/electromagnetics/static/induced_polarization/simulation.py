40c5eec14f24de2f6a2a1f6efaac855dfd0671de
domfournier <dominiquef@mirageoscience.com>
2026-08-31 14:49:55 -0700

    Skip fields computation if stashed

--- simpeg/dask/electromagnetics/static/induced_polarization/simulation.py
+++ simpeg/dask/electromagnetics/static/induced_polarization/simulation.py
@@ -21,6 +21,9 @@ def fields(self, m=None, return_Ainv=False):
     if m is not None:
         self.model = m
 
+    if getattr(self, "_stashed_fields", None) is not None and not return_Ainv:
+        return self._stashed_fields
+
     A = self.getA()
     Ainv = self.solver(A, **self.solver_opts)
     RHS = self.getRHS()
