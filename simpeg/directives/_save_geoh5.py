908548dd951b79097f417444be8da49dfb03f9ba
domfournier <dominiquef@mirageoscience.com>
2026-09-01 08:25:32 -0700

    Change SaveLP directive for more generic name

--- simpeg/directives/_save_geoh5.py
+++ simpeg/directives/_save_geoh5.py
@@ -536,7 +536,7 @@ class SaveModelGroup(SavePropertyGroup):
         return channel_name, component
 
 
-class SaveLPModelGroup(SavePropertyGroup):
+class SaveLPIterationsGroup(SavePropertyGroup):
     """
     Save the model as a property group in the geoh5 file
     """
@@ -562,9 +562,9 @@ class SaveLPModelGroup(SavePropertyGroup):
         channel_name, base_name = super().get_names(component, channel, iteration)
 
         if self.irls_directive.metrics.irls_iteration_count == 0:
-            base_name = "L2 models"
+            base_name = "L2 iterations"
         else:
-            base_name = "LP models"
+            base_name = "LP iterations"
 
         return channel_name, base_name
 
